a3bdf0ed56619ae6e1d7e4f2476dc3ac90a1d5c1
Gabriel Travaini <gabrieltravaini.8039@aluno.saojudas.br>
2022-08-17 19:37:19 -0300

    added a route test

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
 from flask import Flask, render_template
-
 app=Flask('projeto')
 
 @app.route("/")
@@ -9,5 +8,10 @@ def hello_world():
         {"nome":'PC',"preço":"R$15000,00"},
         {"nome":"Deck de magic","preço":"R$700,00"}]
     return render_template("alo.html",n=nome,aProdutos=produtos),200
+
+
+@app.route("/teste")
+def testeroute():
+    return render_template ("testeroute.html"),200
 
 app.run()
