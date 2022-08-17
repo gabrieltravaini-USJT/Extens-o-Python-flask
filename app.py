59b50a3fadbf65c065840a7e7c91fd3329e97602
Gabriel Travaini <gabrieltravaini.8039@aluno.saojudas.br>
2022-08-17 20:30:53 -0300

    added forms

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+
+
 app=Flask('projeto')
 
 @app.route("/")
@@ -19,4 +21,15 @@ def soma(n1=0,n2=0):
     res = int(n1)+int(n2)
     return render_template ("soma.html",r=res),200
 
+@app.route("/form")
+def get_form():
+    return render_template("form.html"),200
+
+@app.route("/form_recebe",methods=["GET","POST"])
+def form_recebe():
+    if request.method == "POST":
+        nome = request.form["nome"]
+        return "Nome: {}".format(nome),200
+    else:
+        return "FEZ ERRADO, BURRO!",200
 app.run()
